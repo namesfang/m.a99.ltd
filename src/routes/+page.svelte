8b67aa31473ed7d399be0d733593758d3e1d74fe
namo.fang <namo.fang@ixiaoshuidi.com>
2023-11-09 13:27:14 +0800

    初始

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export const title = '玖玖记账'
+</script>
+
+<h1>{ title }</h1>
+
+<style lang="scss">
+	h1 {
+		font-size: 1rem;
+		&::before {
+			content: "";
+			border-left: 3px solid #39f;
+			padding-left: 10px;
+		}
+	}
+</style>
